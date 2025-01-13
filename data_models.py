135e811d1c688dd30c80b88186f641809a0f763a
panevka <97028404+panevka@users.noreply.github.com>
2025-01-13 20:20:28 +0100

    Refactor agreement models to keep them compatible with API

--- data_models.py
+++ data_models.py
@@ -1,30 +1,29 @@
-from dataclasses import Field, dataclass
 from datetime import datetime
 from typing import List, Optional
 from enum import Enum
-from pydantic import BaseModel, HttpUrl, PositiveFloat, PositiveInt
+from pydantic import BaseModel, HttpUrl, PositiveFloat, PositiveInt, Field
 from enum import Enum
 
-@dataclass
-class Branch(Enum):
-    "01" = "Dolnoslaskie"
-    "02" = "KujawskoPomorskie"
-    "03" = "Lubelskie"
-    "04" = "Lubuskie"
-    "05" = "Lodzkie"
-    "06" = "Malopolskie"
-    "07" = "Mazowieckie"
-    "08" = "Opolskie"
-    "09" = "Podkarpackie"
-    "10" = "Podlaskie"
-    "11" = "Pomorskie"
-    "12" = "Slaskie"
-    "13" = "Swietokrzyskie"
-    "14" = "WarminskoMazurskie"
-    "15" = "Wielkopolskie"
-    "16" = "Zachodniopomorskie"
+Branch = Enum('Branch', {
+    "Dolnoslaskie": "01",
+"KujawskoPomorskie": "02",
+"Lubelskie": "03",
+"Lubuskie": "04",
+"Lodzkie": "05",
+"Malopolskie": "06",
+"Mazowieckie": "07",
+"Opolskie": "08",
+"Podkarpackie": "09",
+"Podlaskie": "10",
+"Pomorskie": "11",
+"Slaskie": "12",
+"Swietokrzyskie": "13",
+"WarminskoMazurskie": "14",
+"Wielkopolskie": "15",
+"Zachodniopomorskie": "16"
 
-@dataclass
+})
+
 class AgreementAttributes(BaseModel):
     """Represents the attributes of an agreement."""
     code: Optional[str]
@@ -47,12 +46,15 @@ class AgreementAttributes(BaseModel):
         """Configuration for field aliases and other settings."""
         populate_by_name = True
 
+class PageLinks(BaseModel):
+    first_page: Optional[HttpUrl] = Field(alias="first")
+    prev_page: Optional[HttpUrl] = Field(alias="prev")
+    self_page: Optional[HttpUrl] = Field(alias="self") 
+    next_page: Optional[HttpUrl] = Field(alias="next") 
+    last_page: Optional[HttpUrl] = Field(alias="last") 
+    related_pages: Optional[HttpUrl] = Field(alias="related") 
+
 class AgreementLinks(BaseModel):
-    first_page: Optional[HttpUrl] = Field(None, alias="first")
-    prev_page: Optional[HttpUrl] = Field(None, alias="prev")
-    self_page: Optional[HttpUrl] = Field(None, alias="self") 
-    next_page: Optional[HttpUrl] = Field(None, alias="next") 
-    last_page: Optional[HttpUrl] = Field(None, alias="last") 
     related_pages: Optional[HttpUrl] = Field(None, alias="related") 
 
 class Agreement(BaseModel):
