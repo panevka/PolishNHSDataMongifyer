a89e072a89a8dd03480fecd484d89e268fe606af
panevka <97028404+panevka@users.noreply.github.com>
2025-01-18 21:20:17 +0100

    Fix validation issues related to data_models

--- data_models.py
+++ data_models.py
@@ -145,13 +145,13 @@ class Bbox(BaseModel):
     lat2: float
 
 class DataSource(BaseModel):
-    sourcename: str
+    sourcename: Optional[str]
     attribution: str
     license: str
-    url: HttpUrl
+    url: Optional[HttpUrl] = None
 
 class Timezone(BaseModel):
-    name: str
+    name: Optional[str] = None
     offset_STD: str
     offset_STD_seconds: int
     offset_DST: str
@@ -160,8 +160,8 @@ class Timezone(BaseModel):
     abbreviation_DST: str
 
 class Rank(BaseModel):
-    importance: float
-    popularity: float
+    importance: Optional[float] = None
+    popularity: Optional[float] = None
     confidence: float
     confidence_city_level: float
     confidence_street_level: float
@@ -169,35 +169,35 @@ class Rank(BaseModel):
     match_type: str
 
 class QueryParsed(BaseModel):
-    housenumber: Optional[str]
-    street: Optional[str]
-    postcode: Optional[str]
-    city: Optional[str]
-    country: Optional[str]
-    state: Optional[str]
-    expected_type: Optional[str]
+    housenumber: Optional[str] = None
+    street: Optional[str] = None
+    postcode: Optional[str] = None
+    city: Optional[str] = None
+    country: Optional[str] = None
+    state: Optional[str] = None
+    expected_type: Optional[str] = None
 
 class Query(BaseModel):
-    text: Optional[str]
-    housenumber: Optional[str]
-    street: Optional[str]
-    postcode: Optional[str]
-    city: Optional[str]
-    country: Optional[str]
-    state: Optional[str]
+    text: Optional[str] = None 
+    housenumber: Optional[str] = None 
+    street: Optional[str] = None
+    postcode: Optional[str] = None 
+    city: Optional[str] = None
+    country: Optional[str] = None
+    state: Optional[str] = None
     parsed: QueryParsed
 
 class Result(BaseModel):
     datasource: DataSource
-    name: Optional[str]
+    name: Optional[str] = None
     country: Optional[str]
-    country_code: Optional[str]
-    state: Optional[str]
-    district: Optional[str]
-    county: Optional[str]
+    country_code: Optional[str] = None
+    state: Optional[str] = None
+    district: Optional[str] = None
+    county: Optional[str] = None
     city: Optional[str]
-    hamlet: Optional[str]
-    municipality: Optional[str]
+    hamlet: Optional[str] = None
+    municipality: Optional[str] = None
     postcode: Optional[str]
     street: Optional[str]
     lon: float
