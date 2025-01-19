c77badb48422daae040ef180cb7f0bed5f48f88a
panevka <97028404+panevka@users.noreply.github.com>
2025-01-19 14:56:13 +0100

    Create data models for mongodb

--- data_models.py
+++ data_models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from enum import Enum
 from pydantic import BaseModel, HttpUrl, PositiveFloat, PositiveInt, Field, condecimal
 from enum import Enum
@@ -217,7 +217,40 @@ class Response(BaseModel):
     results: List[Result]
     query: Query
 
-    
+# MongoDB models
+
+class AgreementInfo(BaseModel):
+    id: str
+    code: str
+    origin_code: str
+    service_type: str
+    service_name: str
+    amount: PositiveFloat
+    provider_code: str
+    year: int
+
+class ProviderInfo(BaseModel):
+    code: str
+    nip: str
+    regon: str
+    registry_number: str
+    name: str
+    phone: str
+    agreements: List[AgreementInfo]
+
+class Location(BaseModel):
+    type: str = "Point"
+    coordinates: Tuple[ float, float ] 
+
+class ProviderGeoData(BaseModel):
+    code: str
+    city: str
+    street: str
+    building_number: str
+    commune: str
+    post_code: str
+    voivodeship: Branch
+    location: Location
 
 
  
